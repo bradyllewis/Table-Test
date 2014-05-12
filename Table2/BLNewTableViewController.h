ae72221e7006c8e5716edc91173ae87637ddeee1
Brady Lewis <bradyllewis@Bradys-Mac-mini.local>
2014-05-12 16:01:00 -0400

    first commit

---
+++ Table2/BLNewTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  BLNewTableViewController.h
+//  Table2
+//
+//  Created by Brady Lewis on 5/12/14.
+//  Copyright (c) 2014 Brady Lewis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLNewTableViewController : UITableViewController
+
+@end
